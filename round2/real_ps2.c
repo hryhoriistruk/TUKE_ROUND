b906e94042c68bc9cd249c30dd6dbc950f4e1079
Hryhorii_Struk <136825012+hryhoriistruk@users.noreply.github.com>
2024-11-20 19:25:46 +0200

    Update real_ps2.c

--- round2/real_ps2.c
+++ round2/real_ps2.c
@@ -188,13 +188,7 @@ int is_prime(const int num){
     }
     return 1;
 }
-int leap_year(int year) {
 
-    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-        return 1;
-    }
-    return 0;
-    }
 
 
 int leap_year(int year) {
