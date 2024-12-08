6ebb1b86a23c7b65d62cf79a1745c3ebbf6f83c7
Hryhorii_Struk <136825012+hryhoriistruk@users.noreply.github.com>
2024-12-08 20:57:26 +0100

    Add files via upload

--- round3/round3.c
+++ round3/round3.c
@@ -1,160 +1,182 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 
-int max_2d(const int size, int array[][size]){
-    if(array == NULL){
-        return -1;
+int max_2d(const int size, int array[][size]) {
+  if (array == NULL) {
+    return -1;
+  }
+
+  int max = array[0][0];
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      if (array[i][j] > max) {
+        max = array[i][j];
+      }
     }
-    int max = array[0][0];
-    for (int i = 0; i < 4; i++)
-    {/*  */
-        for (int j = 0; j < 4; j++)
-        {
-            if(max < array[i][j]){
-                max = array[i][j];
-            }
-        }
-    }
-    return max;
+  }
+  return max;
 }
 
+bool contains_char_2d(char array[], char value) {
+  if (array == NULL) {
+    return false;
+  }
 
-int vowels_count_2d(const int rows, const int cols, char string[][cols]){
-    if(string == NULL){
-        return -1;
+  for (int i = 0; i < strlen(array); i++) {
+    if (array[i] == value) {
+      return true;
     }
-    int count = 0;
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            if(string[i][j] == 'a' || string[i][j] == 'A' || string[i][j] == 'i' || string[i][j] == 'I' || string[i][j] == 'o' || string[i][j] == 'O' || string[i][j] == 'e' || string[i][j] == 'E' || string[i][j] == 'u' || string[i][j] == 'U'){
-                count++;
-            }
-        }
-    }
-    return count;
+  }
+  return false;
 }
 
-int is_in_array_2d(const int num, const int size, int array[][size]){
-    if(array == NULL){
-        return -1;
-    }
+int vowels_count_2d(const int rows, const int cols, char string[][cols]) {
+  if (string == NULL) {
+    return -1;
+  }
 
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            if(num == array[i][j]){
-                return 1;
-            }
-        }
+  int count = 0;
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      if (contains_char_2d("aeiouAEIOU", string[i][j])) {
+        count++;
+      }
     }
-    return 0;
+  }
+  return count;
 }
 
-int largest_line(const int size, int array[][size]){
-    if(array == NULL){
-        return -1;
-    }
+int is_in_array_2d(const int num, const int size, int array[][size]) {
+  if (array == NULL) {
+    return -1;
+  }
 
-    int k = 0;
-    int return_index;
-    for (int i = 0; i < size; i++)
-    {
-        int count = 0;
-        for (int j = 0; j < size; j++)
-        {
-            count = count + array[i][j];
-        }
-        if(i == 0){
-            k = count;
-            return_index = 0;
-        }
-        else{
-            if(k < count){
-                k = count;
-                return_index = i;
-            }
-        }
-
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      if (array[i][j] == num) {
+        return 1;
+      }
     }
-    return return_index;
+  }
+  return 0;
 }
 
-void swap_case_2d(const int size, char array[][size]){
-    if(array == NULL){
-        return;
+int largest_line(const int size, int array[][size]) {
+  if (array == NULL) {
+    return -1;
+  }
+
+  int max_sum = NULL;
+  int max_line_index = 0;
+
+  for (int i = 0; i < size; i++) {
+    int sum = 0;
+    for (int j = 0; j < size; j++) {
+      sum += array[i][j];
     }
-    int temp = 10;
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            if(isalpha){
-                if(isupper(array[i][j])){
-                    array[i][j] = tolower(array[i][j]);
-                }
-                else{
-                    array[i][j] = toupper(array[i][j]);
-                }
-            }
-        }
+    if (max_sum == NULL) {
+      max_sum = sum;
     }
+    else if (sum > max_sum) {
+      max_sum = sum;
+      max_line_index = i;
+    }
+  }
+
+  return max_line_index;
 }
 
-int largest_col_2d(int size, int array[][size]){
-    if(array == NULL){
-        return -1;
+void swap_case_2d(const int rows, const int cols, char string[][cols]) {
+  if (string == NULL) {
+    return;
+  }
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      if (isalpha(string[i][j])) {
+        string[i][j] = islower(string[i][j]) ? toupper(string[i][j]) : tolower(string[i][j]);
+      }
     }
-    int biggest = 0;
-    int biggest_col_index = 0;
-    for (int i = 0; i < size; i++)
-    {
-        int sum = 0;
-        for (int j = 0; j < size; j++)
-        {
-            sum = sum + array[j][i];
-        }
-        if(biggest < sum){
-            biggest = sum;
-            biggest_col_index = i;
-        }
-    }
-    return biggest_col_index;
+  }
 }
 
+int largest_col(const int size, int array[][size]) { // Те саме, що і largest_line, але поміняні лише два символи
+  if (array == NULL) {
+    return -1;
+  }
 
-int main(){
-    int array[4][4] =   {
-            {1, 2, 30, 4},
-            {2, 5, 3, 4},
-            {3, 20, 3, 40},
-            {4, -1, -3, 44}
-    };
-    char vowels[6][6] = {
-            "AAAAAA",
-            "aaddaa",
-            "aadda-",
-            "UeddiO",
-            "aaddio",
-            "aaddio",
-    };
-    printf("max value in 2d array: %d\n", max_2d(4, array));
-    printf("vowels count: %d\n", vowels_count(6, 6, vowels));
-    printf("is_is_array_2d: %d\n", is_in_array_2d(10, 4, array));
-    printf("largest line: %d", largest_line(4, array));
-    int size = 6;
-    swap_case_2d(size, vowels);
-    printf("Swap case 2d:\n");
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            printf("%c", vowels[i][j]);
-        }
-        printf("\n");
+  int max_sum = NULL;
+  int max_col_index = 0;
+  for (int i = 0; i < size; i++) {
+    int sum = 0;
+    for (int j = 0; j < size; j++) {
+      sum += array[j][i]; // [j][i] замість [i][j]
     }
-    printf("Largest col 2d: %d\n", largest_col_2d(4, array));
+    if (max_sum == NULL) {
+      max_sum = sum;
+    }
+    else if (sum > max_sum) {
+      max_sum = sum;
+      max_col_index = i;
+    }
+  }
+  return max_col_index;
 }
 
+int conunt_zeroes_2d(const int size, int array[][size]) {
+  if (array == NULL) {
+    return -1;
+  }
+
+  int count = 0;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      if (array[i][j] == 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+void swap_sign_2d(const int size, int array[][size]) {
+  if (array == NULL) {
+    return;
+  }
+
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      array[i][j] = -array[i][j];
+    }
+  }
+}
+
+int main() {
+
+  char strings[3][50] = {
+    "hello WORL###D!",
+    "aHOJ",
+    "Ahoj"
+  };
+  printf("Vowels count: %d\n", vowels_count_2d(3, 50, strings));
+  // 7
+
+  int array[2][2] = { {1, 0}, {0, -3} };
+  printf("%d %d\n", is_in_array_2d(100, 2, array), is_in_array_2d(1, 2, array));
+  // 0 1
+
+  printf("%d\n", largest_line(2, array));
+  // 0
+
+  swap_case_2d(3, 50, strings);
+  for (int i = 0; i < 3; i++) {
+    printf("%s\n", strings[i]);
+  }
+
+  printf("%d\n", largest_col(2, array));
+
+  return 0;
+}
